2e05d9a1fb4262938ef39cb92e5e70b731cfb123
shubhamsatam <113004440+shubhamsatam@users.noreply.github.com>
2022-09-12 13:41:22 +0530

    Add files via upload

---
+++ oddnumber1.java
@@ -0,0 +1,16 @@
+
+public class oddnumber1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i=1;i<=20;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i+"\t");
+	}
+
+}
+	}
+}
