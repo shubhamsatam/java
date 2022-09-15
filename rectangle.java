426c316e72fb563c701cb45d35459ef1305af1ea
shubhamsatam <113004440+shubhamsatam@users.noreply.github.com>
2022-09-15 13:54:13 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,28 @@
+
+public class rectangle {
+	
+int length,width;
+
+
+rectangle()
+{
+	
+	length=10;
+	width=20;
+	int area=width*length;
+	System.out.println("Area of rectangle="+area);
+}
+rectangle(int length,int width)
+{
+	this.lenght=length;
+	this.width=width;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+rectangle r=new rectangle();
+
+	}
+
+}
---
+++ sum.java
@@ -0,0 +1,18 @@
+
+public class sum {
+static void addNumbers()
+{
+	int a=0;
+	for(int i=1;i<=100;i++)
+	{
+		a=a+i;
+		
+	}
+	System.out.println("Sum of first 100 numbers is"+a);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+addNumbers();
+	}
+
+}
